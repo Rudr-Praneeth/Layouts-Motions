7b4a5cb4d3a90e4f4e3c3111784c75307ce8630b
Rudr-Praneeth <rudrith.vyom@gmail.com>
2026-05-14 17:12:18 +0530

    PROJECT INITIALISATION

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
+import Dashboard from "./projects/Layouts/Dashboard"
 
 const App = () => {
   return (
     <div className="h-screen bg-black/90 p-4">
-      
+      <Dashboard />
     </div>
   )
 }
