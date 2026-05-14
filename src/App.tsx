23613c4172875e5c4b51fcce61e5943524c4cbbe
Praneeth Narayan <rudrith.vyom@gmail.com>
2026-05-14 17:15:54 +0530

    STRUCTURE INITIALISATION (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Dashboard from "./projects/Layouts/Dashboard"
 
 const App = () => {
   return (
-    <div className="h-screen bg-black/90 p-4">
+    <div>
       <Dashboard />
     </div>
   )
